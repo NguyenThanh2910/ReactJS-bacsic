a1068bb95d32393f6a1a61eeb43e52ebb2c4d354
Nguyen Thanh <thanh.nn@mor.com.vn>
2024-10-21 18:50:05 +0700

    fix: Fix duplicate code, UI bugs and character limits

--- src/calculator.js/Calculator.js
+++ src/calculator.js/Calculator.js
@@ -7,51 +7,55 @@ const Calculator = () => {
 
   const isValidInput = (value) => {
     const invalidPatterns = [
-      /\*\*/,
-      /\*\/|\/\*/,
+      /\*\*/g,
+      /\*\/|\/\*/g,
       /\+\*/g,
       /\-\*/g,
       /\*\+/g,
-      /\/\//,
-      /\-\//,
-      /\+\//,
+      /\/\//g,
+      /\-\//g,
+      /\+\//g,
       /\/\+/g,
       /\+\+/g,
       /\-\-/g,
       /\-\+/g,
     ];
-    return !invalidPatterns.some((parent)=> parent.test(value));
+    return !invalidPatterns.some((pattern) => pattern.test(value));
   };
+
   const handleButtonClick = (value) => {
     if (isResult) {
-        if (["+", "-", "*", "/"].includes(value)) { 
-          setInput(result + value);
-        } else {
-          setInput(value);
-        }
-        setResult("");
-        setIsResult(false); 
+      if (["+", "-", "*", "/"].includes(value)) {
+        setInput(result + value);
       } else {
-        const newInput = input + value;
-        if (isValidInput(newInput)) {
-          setInput(newInput);
-        }
+        setInput(value);
       }
+      setResult("");
+      setIsResult(false);
+    } else {
+      const newInput = input + value;
+      if (newInput.length <= 18 && isValidInput(newInput)) {
+        setInput(newInput);
+      }
+    }
   };
-  const calculatorResult =()=>{
+
+  const calculateResult = () => {
     try {
-        const evaluatedResult = eval(input);
-        setResult(evaluatedResult);
-        setIsResult(true); 
-      } catch (error) {
-        setResult("error");
-      }
-  }
-  const clearInput=()=>{
-    setInput('');
-    setResult('');
+      const evaluatedResult = eval(input);
+      setResult(evaluatedResult);
+      setIsResult(true);
+    } catch {
+      setResult("error");
+    }
+  };
+
+  const clearInput = () => {
+    setInput("");
+    setResult("");
     setIsResult(false);
-  }
+  };
+
   const handleSquare=()=>{
     try {
         if(input){
@@ -86,121 +90,42 @@ const Calculator = () => {
         setInput("error")
     }
   }
-  
+
+  const Button = ({ label, onClick, className }) => (
+    <button
+      className={`bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200 ${className}`}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
 
   return (
     <div className="max-w-xs mt-7 mx-auto bg-slate-600 p-6 rounded-lg shadow-lg">
       <div className="flex flex-col mb-4">
-        <div className="border border-gray-300 p-4 text-right text-2xl bg-gray-100 rounded-md overflow-x-auto whitespace-nowrap text-ellipsis">
+        <div className="border border-gray-300 p-4 text-right text-2xl bg-gray-200 rounded-md overflow-x-auto whitespace-nowrap text-ellipsis">
           {input}
         </div>
-        <div className="border border-gray-300 p-4 text-right text-2xl text-gray-600 bg-gray-200 rounded-md overflow-x-auto whitespace-nowrap text-ellipsis">
+        <div className="border border-gray-300 p-4 text-right text-2xl bg-gray-200 rounded-md overflow-x-auto whitespace-nowrap text-ellipsis">
           {result}
         </div>
       </div>
       <div className="grid grid-cols-4 gap-4">
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleButtonClick("1")}
-        >
-          1
-        </button>
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleButtonClick("2")}
-        >
-          2
-        </button>
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleButtonClick("3")}
-        >
-          3
-        </button>
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleButtonClick("+")}
-        >
-          +
-        </button>
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleButtonClick("4")}
-        >
-          4
-        </button>
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleButtonClick("5")}
-        >
-          5
-        </button>
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleButtonClick("6")}
-        >
-          6
-        </button>
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleButtonClick("-")}
-        >
-          -
-        </button>
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleButtonClick("7")}
-        >
-          7
-        </button>
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleButtonClick("8")}
-        >
-          8
-        </button>
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleButtonClick("9")}
-        >
-          9
-        </button>
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleButtonClick("*")}
-        >
-          *
-        </button>
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleButtonClick("0")}
-        >
-          0
-        </button>
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleSquareRoot()}
-        >
-          √
-        </button>
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleSquare()}
-        >
-          x²
-        </button>
-        <button
-          className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-200"
-          onClick={() => handleButtonClick("/")}
-        >
-          /
-        </button>
-        <button className="bg-green-500 text-white p-3 rounded-lg hover:bg-green-600 transition duration-200 col-span-2" onClick={calculatorResult}>
-          =
-        </button>
-        <button className="bg-red-500 text-white p-3 rounded-lg hover:bg-red-600 transition duration-200" onClick={clearInput}>
-          C
-        </button>
+        {["1", "2", "3", "+"].map((val) => (
+          <Button key={val} label={val} onClick={() => handleButtonClick(val)} />
+        ))}
+        {["4", "5", "6", "-"].map((val) => (
+          <Button key={val} label={val} onClick={() => handleButtonClick(val)} />
+        ))}
+        {["7", "8", "9", "*"].map((val) => (
+          <Button key={val} label={val} onClick={() => handleButtonClick(val)} />
+        ))}
+        <Button label="0" onClick={() => handleButtonClick("0")} />
+        <Button label="√" onClick={() => handleSquareRoot()} />
+        <Button label="x²" onClick={() => handleSquare()} />
+        <Button label="/" onClick={() => handleButtonClick("/")} />
+        <Button label="=" className="col-span-2 bg-green-500 hover:bg-green-700 " onClick={calculateResult} />
+        <Button label="C" className='bg-red-500 hover:bg-red-700' onClick={clearInput} />
       </div>
     </div>
   );
